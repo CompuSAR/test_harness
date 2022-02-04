f1650da0f2ffd5a73190bd8a6bd81427a8079f5b
Shachar Shemesh <shachar@shemesh.biz>
2022-02-04 10:34:12 +0200

    Translate assembly to Mem file

--- test_program/test_program.s
+++ test_program/test_program.s
@@ -1,20 +1,31 @@
+    .org $00a9
+lda_zp_test: .byte $07
+
     .org $0300
 
-    .list
+start:
     nop
-    .ifdef CPU_WDC
     nop
-    .fi
     nop
     lda #$03
-    lda #$05
-    lda $a9
-    nop
-    lda $8623
-    nop
+    lda lda_zp_test     ; Make sure we don't treat the operand as an opcode
 
-    .org $00
-    .ascii "Hello, world",10,"How are you?",0
-    .blk 12, 1
+reset_handler:
+    php
+    ldx #$ff
+    txs
+    lda #start/256
+    pha
+    lda #start%256
+    pha
+    lda #0
+    pha
+    rti
 
+int_handler:
+nmi_handler:
 
+    .org $fffa
+    .word nmi_handler
+    .word reset_handler
+    .word int_handler
