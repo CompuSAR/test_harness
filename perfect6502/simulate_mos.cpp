97c9938fa4eb9f7470058bf06dcdac9f985d8f78
Shachar Shemesh <shachar@shemesh.biz>
2023-08-21 12:10:51 +0300

    Test plan successfully generated

--- perfect6502/simulate_mos.cpp
+++ perfect6502/simulate_mos.cpp
@@ -7,10 +7,8 @@ extern "C" {
 //#include "cpu/netlist_6502.h"
 }
 
-#include <fstream>
-#include <functional>
+#include <set>
 #include <unordered_map>
-#include <vector>
 
 /* Output memory file:
  31-16: address
@@ -28,7 +26,7 @@ struct ExtSignal {
 enum { ExtReady, ExtSo, ExtNmi, ExtReset, ExtIrq };
 ExtSignal signals[5];
 
-void record_bus( void *cpu_state, std::ostream &logger ) {
+void record_bus( void *cpu_state, std::ostream &logger, const char *comment ) {
     logger<<"1_";
 
     char buffer[5];
@@ -39,33 +37,40 @@ void record_bus( void *cpu_state, std::ostream &logger ) {
     logger<<buffer<<"_";
 
     if( readRW(cpu_state) )
-        logger<<"01\n";
+        logger<<"01";
     else
-        logger<<"00\n";
+        logger<<"00";
+
+    if( comment ) {
+        logger<<"\t\t// "<<comment;
+    }
+
+    logger<<"\n";
 }
 
-std::unordered_map<size_t, std::vector< std::function<void()> >> callbacks;
+struct CycleTask {
+    std::set<nodenum_t> raise, lower;
+};
+
+std::unordered_map<size_t, CycleTask> cycleTasks;
 size_t cycle_num = 0;
 
-void add_callback(size_t cycle, std::function<void()> cb) {
-    auto inserter = callbacks.emplace( cycle, std::vector< std::function<void()> >() );
-    inserter.first->second.push_back( cb );
-}
-
-void schedule_signal_change(void *cpu_state, size_t delay, size_t count, nodenum_t node) {
-    add_callback(cycle_num+delay,
-            [cpu_state, count, node]() {
-                setNode( cpu_state, node, false );
-                add_callback( cycle_num+count, [node, cpu_state]() {
-                            setNode( cpu_state, node, true );
-                        });
-            });
+void schedule_signal_change(size_t delay, size_t count, nodenum_t node, bool polarity) {
+    size_t start_cycle = cycle_num + delay;
+    size_t end_cycle = start_cycle + count;
+    std::cerr<<"Scheduling IO: "<<cycle_num<<" node "<<node<<" polarity "<<polarity<<" start "<<start_cycle<<" end "<<end_cycle<<"\n";
+    cycleTasks.try_emplace(polarity ? start_cycle : end_cycle).first->second.raise.insert(node);
+    cycleTasks.try_emplace(polarity ? end_cycle : start_cycle).first->second.lower.insert(node);
 }
 
 int main(int argc, char *argv[]) {
+    if( argc<2 ) {
+        std::cerr<<"Usage: "<<argv[0]<<" test_program.mem\n";
+
+        return 2;
+    }
     ReadMem<8> test_program_mem(argv[1]);
     std::unordered_map<size_t, std::string> commented_lines;
-    std::ofstream recorded_bus(argv[2]);
 
     static constexpr nodenum_t
             RDY = 89,
@@ -84,40 +89,49 @@ int main(int argc, char *argv[]) {
 
     void *state = initAndResetChip();
 
+    setNode(state, SO, true);
+    setNode(state, NMI, true);
+
     bool clk = false;
     bool recording = false;
     bool done = false;
     while( !done ) {
         step(state);
 
-        std::cout<<cycle<<"\n";
         if( clk && readAddressBus(state)==0xfffc )
             recording = true;
 
         if( recording && clk ) {
-            record_bus(state, recorded_bus);
+            auto comment = commented_lines.find(readAddressBus(state));
+            record_bus(state, std::cout, comment!=commented_lines.end() ? comment->second.c_str() : nullptr);
 
-            auto callback_vect = callbacks.find(cycle_num);
-            if( callback_vect != callbacks.end() ) {
-                for( auto &callback : callback_vect->second ) {
-                    callback();
+            auto tasks_iter = cycleTasks.find(cycle_num);
+            if( tasks_iter != cycleTasks.end() ) {
+                for( auto node : tasks_iter->second.raise ) {
+                    std::cerr<<"Raise "<<node<<"\n";
+                    setNode(state, node, true);
                 }
-                callbacks.erase(callback_vect);
+                for( auto node : tasks_iter->second.lower ) {
+                    std::cerr<<"Lower "<<node<<"\n";
+                    setNode(state, node, false);
+                }
+
+                cycleTasks.erase(tasks_iter);
             }
 
             if( (readAddressBus(state) & 0xff00) == 0x0200 && readRW(state)==0 ) {
                 switch( readAddressBus(state)&0xff ) {
                 case 0x00: done = true; break;
                 case 0x80: signals[ExtReady].count = readDataBus(state); break;
-                case 0x81: schedule_signal_change(state, readDataBus(state), signals[ExtReady].count, RDY); break;
+                case 0x81: schedule_signal_change(readDataBus(state), signals[ExtReady].count, RDY, false); break;
                 case 0x82: signals[ExtSo].count = readDataBus(state); break;
-                case 0x83: schedule_signal_change(state, readDataBus(state), signals[ExtSo].count, SO); break;
+                case 0x83: schedule_signal_change(readDataBus(state), signals[ExtSo].count, SO, false); break;
                 case 0xfa: signals[ExtNmi].count = readDataBus(state); break;
-                case 0xfb: schedule_signal_change(state, readDataBus(state), signals[ExtNmi].count, NMI); break;
+                case 0xfb: schedule_signal_change(readDataBus(state), signals[ExtNmi].count, NMI, false); break;
                 case 0xfc: signals[ExtReset].count = readDataBus(state); break;
-                case 0xfd: schedule_signal_change(state, readDataBus(state), signals[ExtReset].count, RES); break;
+                case 0xfd: schedule_signal_change(readDataBus(state), signals[ExtReset].count, RES, false); break;
                 case 0xfe: signals[ExtIrq].count = readDataBus(state); break;
-                case 0xff: schedule_signal_change(state, readDataBus(state), signals[ExtIrq].count, IRQ); break;
+                case 0xff: schedule_signal_change(readDataBus(state), signals[ExtIrq].count, IRQ, false); break;
                 }
             }
 
